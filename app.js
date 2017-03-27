c728235c62571e5294f22ffdc3a87eab7ca95acb
Ubuntu <ubuntu@ip-10-0-1-123.us-west-2.compute.internal>
2017-03-27 02:06:16 +0000

    6 push

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello my boys 5.0");
+  res.send("Hello my boys 6.0");
 });
 
 app.listen(4000);
